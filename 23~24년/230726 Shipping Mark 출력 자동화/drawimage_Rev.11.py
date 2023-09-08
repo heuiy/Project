33e740a7463e90a724f1688964e67caa7ef6a2ca
heuiy <52515917+heuiy@users.noreply.github.com>
2023-09-08 14:38:09 +0900

    ㅇ

--- 23~24년/230726 Shipping Mark 출력 자동화/drawimage_Rev.11.py
+++ 23~24년/230726 Shipping Mark 출력 자동화/drawimage_Rev.11.py
@@ -1,4 +1,4 @@
-# 수정 중
+# 전체 페이지 수를 5개 이상의 작은 배치로 계속 나눔
 
 # WMS 와 이름 맞추기
 # STANDARD_CHNE             이브아르_중문
@@ -101,47 +101,28 @@ def select_type():
 
 def main():
     total_documents = int(input("전체 문서 번호를 입력하세요 (예: 30): "))
-    first_pdf_copies = int(input("첫 번째 출력되는 PDF 페이지 수를 입력하세요 (예: 20): "))
-
-    if first_pdf_copies > total_documents:
-        print("첫 번째 PDF 페이지 수는 전체 문서 번호보다 클 수 없습니다. 다시 시도해주세요.")
-        return
-
     current_document_number = 1
 
-    # 첫 번째 PDF 파일 처리
-    selected_pdf_file = select_pdf_file()
-    writer = copy_pages(selected_pdf_file, first_pdf_copies)
-    if writer is None:
-        print("PDF 생성에 실패하였습니다.")
-    else:
-        temp_pdf_file = os.path.join("C:\\Users\\LG\\Downloads", "temp.pdf")
-        with open(temp_pdf_file, "wb") as temp_pdf:
-            writer.write(temp_pdf)
+    while current_document_number <= total_documents:
+        # 남은 문서 수 계산
+        remaining_documents = total_documents - current_document_number + 1
+        
+        print(f"남은 문서 수: {remaining_documents}")
+        
+        # 현재 배치에 복사할 페이지 수 입력
+        current_batch_copies = int(input(f"{current_document_number}부터 시작하는 현재 배치를 몇 장으로 할까요? "))
+        
+        if current_batch_copies > remaining_documents:
+            print("입력한 배치 페이지 수가 남은 문서 수보다 클 수 없습니다. 다시 시도해주세요.")
+            continue
 
-        type_choice = select_type()
-
-        # temp.pdf 파일에 워터마크 추가
-        writer_with_watermark = add_watermark(temp_pdf_file, type_choice, current_document_number, total_documents)
-
-        # 파일 이름에 current_document_number 추가
-        timestamp = datetime.now().strftime("%Y%m%d%H%M")
-        output_pdf_file = os.path.join("C:\\Users\\LG\\Downloads", f"{timestamp}_{current_document_number}.pdf")
-        with open(output_pdf_file, "wb") as output_pdf:
-            writer_with_watermark.write(output_pdf)
-
-        print(f"페이지 번호가 추가된 파일이 저장되었습니다: {output_pdf_file}")
-
-        # 다음 문서 번호로 업데이트
-        current_document_number += first_pdf_copies + (1 if writer.getNumPages() == 2 else 0)
-
-    # 두 번째 PDF 파일 처리 (첫 번째 PDF 파일이 전체 문서 번호를 채우지 않았을 경우)
-    if current_document_number <= total_documents:
-        second_pdf_copies = total_documents - current_document_number + 1
+        # PDF 파일 선택 및 복사
         selected_pdf_file = select_pdf_file()
-        writer = copy_pages(selected_pdf_file, second_pdf_copies)
-
-        # 두 번째 PDF 파일에 대한 나머지 처리
+        writer = copy_pages(selected_pdf_file, current_batch_copies)
+        if writer is None:
+            print("PDF 생성에 실패하였습니다.")
+            continue
+        
         temp_pdf_file = os.path.join("C:\\Users\\LG\\Downloads", "temp.pdf")
         with open(temp_pdf_file, "wb") as temp_pdf:
             writer.write(temp_pdf)
@@ -152,14 +133,17 @@ def main():
         writer_with_watermark = add_watermark(temp_pdf_file, type_choice, current_document_number, total_documents)
         
         # 파일 이름에 current_document_number 추가
+        timestamp = datetime.now().strftime("%Y%m%d%H%M")
         output_pdf_file = os.path.join("C:\\Users\\LG\\Downloads", f"{timestamp}_{current_document_number}.pdf")
         with open(output_pdf_file, "wb") as output_pdf:
             writer_with_watermark.write(output_pdf)
 
         print(f"페이지 번호가 추가된 파일이 저장되었습니다: {output_pdf_file}")
 
+        # 다음 문서 번호로 업데이트
+        current_document_number += current_batch_copies + (1 if writer.getNumPages() == 2 else 0)
+
     print("모든 문서 번호가 추가되었습니다.")
 
 if __name__ == "__main__":
     main()
-
